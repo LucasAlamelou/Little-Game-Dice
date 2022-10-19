53ef8f251af8018765cd47dd41d0540c16a421c0
LucasAlamelou <lucas.alamelou@gmail.com>
2022-10-19 13:08:20 +0200

    add html and css (first commit)

---
+++ main.js
@@ -0,0 +1,20 @@
+// Button relancer une partie
+const btnNewGame = document.getElementById('new-game');
+
+// Joueurs
+const playerOne = document.getElementById('player1');
+const playerTwo = document.getElementById('player2');
+
+// Scores des joueurs de la parties
+const scorePlayerOne = document.getElementById('score-player1');
+const scorePlayerTwo = document.getElementById('score-player2');
+
+// Bouton lancer le dé
+const btnRollDice = document.getElementById('btn-roll');
+
+// Button recolter le score
+const btnHold = document.getElementById('btn-hold');
+
+// Scores des joueurs du round
+const roundPlayerOne = document.getElementById('round-player1');
+const roundPlayerTwo = document.getElementById('round-player2');
